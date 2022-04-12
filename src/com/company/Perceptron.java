861251fa25b285ebd80a7a583a108e17a610af1e
s23109 <s23109@pjwstk.edu.pl>
2022-04-12 12:37:33 +0200

    Normalizacja wag perceptronu, dodanie nazwy w klasie perceptronu

--- src/com/company/Perceptron.java
+++ src/com/company/Perceptron.java
@@ -6,17 +6,20 @@ import java.util.List;
 public class Perceptron {
 
     /*
-        Wariant versicolor = 0
-        Wariant virginica = 1
-    */
+
+    Dla 1 - Neuron activation
+    Dla 0 - Neuron ignored
+
+     */
 
     final double stala_uczenia ;
     final double akceptowalny_procent_bladow ;
     double prog_odchylenia = 3 ;
     private List<Double> wagi = new ArrayList<>();
     private int number_of_wagi ;
+    String Perceptron_name;
 
-    public Perceptron(int number_of_wagi , double stala_uczenia , double procent_bledow) {
+    public Perceptron(int number_of_wagi , double stala_uczenia , double procent_bledow , String perceptron_name) {
         this.number_of_wagi = number_of_wagi;
         System.out.print("Wagi Perceptronu: ");
         for (int i = 0; i < number_of_wagi; i++) {
@@ -25,7 +28,8 @@ public class Perceptron {
         }
         this.stala_uczenia=stala_uczenia;
         this.akceptowalny_procent_bladow=procent_bledow;
-        System.out.println("\nPerceptron created\n------------");
+        this.Perceptron_name = perceptron_name;
+        System.out.println("\nPerceptron "+ this.Perceptron_name + " created\n------------");
     }
 
     public void wypisz_wagi (){
@@ -54,10 +58,7 @@ public class Perceptron {
     }
 
     public int guess (List<Double> tested){
-        /*
-        Wariant versicolor = 0
-        Wariant virginica = 1
-         */
+
         Double net = 0.0;
 
         for (int i = 0; i < tested.size(); i++) {
@@ -67,14 +68,43 @@ public class Perceptron {
         net+=prog_odchylenia;
 
         if (net >= 0) {
-            //virginica
+            // neuron activation
             return 1;
         }
-        //versicolor
+        //not recognised
         return 0;
     }
 
+    private Double dlugosc (){
+        double dlugosc = 0;
 
+        for (Double ele: wagi
+             ) {
+            dlugosc+= (ele*ele);
+        }
+        dlugosc= Math.sqrt(dlugosc);
+
+        return dlugosc;
+
+    }
+
+    public void normalizuj_wagi (){
+
+        List<Double> nowa_waga = new ArrayList<>();
+        double Dlugosc = dlugosc();
+
+        for (Double ele :wagi
+             ) {
+            nowa_waga.add(ele/Dlugosc);
+        }
+
+        this.wagi=nowa_waga;
+
+    }
+
+    public String getPerceptron_name() {
+        return Perceptron_name;
+    }
 
 
 }
